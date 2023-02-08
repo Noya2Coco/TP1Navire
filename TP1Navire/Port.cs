4d31de7ac72dea973f6ac722eed90d471e12b23a
noah.rousseau <noah.rousseau@CP-SIO-W10-PG03>
2023-02-08 11:42:45 +0100

    Ajoutez des fichiers projet.

---
+++ TP1Navire/Port.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1Navire
+{
+    class Port<T>
+    {
+        private string nom;
+        private int nbNaviresMax;
+        private List<T> navires = new List<T>();
+
+        public Port (string nom)
+        {
+            this.nom = nom;
+            this.nbNaviresMax = 5;
+        }
+
+        public int NbNaviresMax { get => nbNaviresMax; set => nbNaviresMax = value; }
+
+
+        private bool portPlein(Port port)
+        {
+            return !(port.nbNaviresMax == port.Count);
+        }
+
+        private bool portVide(Port port)
+        {
+            return (port.Count == 0);
+        }
+
+        public void EnregistrerArrivee(Navire navire)
+        {
+            if (!portPlein(this))
+            {
+                this.navires.Add(navire);
+            }
+            else
+            {
+                Console.WriteLine("Ajout impossible, le port est complet.");
+            }
+        }
+
+        public void EnregistrerDepart(string imo)
+        {
+            if 
+        }
+
+        public bool EstPresent(string imo)
+        {
+            return this.navires.Contains(imo);
+        }
+
+        
+    }
+}
---
+++ TP1Navire/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TP1Navire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesterInstanciations();
+
+            Console.ReadKey();
+        }
+
+        public static void TesterInstanciations()
+        {
+            // déclaration de l'object unNvaire de la classe Navire
+            Navire unNavire;
+            // Instantiation
+            unNavire = new Navire("IM09427639", "Copper Spirit", "Hydrocarbures", 156827);
+            Affiche(unNavire);
+            // Déclaration et instantiation d'un autre objet de la classe Navire
+            Navire unAutreNavire = new Navire("IMO09839272", "MSC Isabella", "Porte-conteneurs", 197500);
+            Affiche(unAutreNavire);
+            // Nouvelle déclaration de l'autre navire
+            unAutreNavire = new Navire("IMO08715871", "MSC Pilar");
+            Affiche(unAutreNavire);
+        }
+
+        public static void Affiche(Navire navire)
+        {
+            Console.WriteLine("\n Identification :   {0} \n " +
+                                 "Nom :              {1} \n " +
+                                 "Type de frêt :     {2} \n " +
+                                 "Quantité de frêt : {3} \n " +
+                                 "_____________________________", navire.Imo, navire.Nom, navire.LibelleFret, navire.QteFretMaxi.ToString());
+        }
+
+        public static vois
+    }
+}
